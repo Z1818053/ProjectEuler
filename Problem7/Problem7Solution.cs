75ddd9acfc2548c315ba627c6ce44f7b6658c04d
Matthew Rycraft <mattrycraft@comcast.net>
2020-04-08 11:00:04 -0500

    Finished isPrime method

---
+++ Problem7/Problem7Solution.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Problem7
+{
+    class Problem7Solution
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static bool isPrime(int number)
+        {
+            if (number <= 1) { return false; }
+            else if (number == 2) { return true; }
+            else if (number % 2 == 0) { return false; }
+
+            int counter = 3;
+
+            while ((counter * counter) <= number)
+            {
+                if (number % counter == 0){ return false; }
+                else
+                {
+                    counter += 2;
+                }
+            }
+
+            return true;
+        }
+    }
+}
