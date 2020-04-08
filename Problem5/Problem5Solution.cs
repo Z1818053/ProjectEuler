32155022e8aa5e821ec541b08d5099dbef300bf9
Matthew Rycraft <mattrycraft@comcast.net>
2020-04-08 10:39:48 -0500

    Project Euler Problem 5 (could potentially use optimization)

---
+++ Problem5/Problem5Solution.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Problem5
+{
+    class Problem5Solution
+    {
+        static void Main(string[] args)
+        {
+            long counter = 1, result = 0;
+            bool smallestMultiple = false;
+
+            while (!smallestMultiple)
+            {
+                for (int i = 1; i <= 20; i++)
+                {
+                    if (counter % i == 0)
+                    {
+                        smallestMultiple = true;
+                        result = counter;
+                    }
+                    else
+                    {
+                        smallestMultiple = false;
+                        counter++;
+                        break; 
+                    }
+                }
+            }
+
+            Console.WriteLine("The Smallest Multliple divible by 1 - 20 is: " + result);
+            
+        }
+    }
+}
