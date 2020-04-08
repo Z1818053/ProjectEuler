6ba9f4bdc74bf51e38e927f8ed6eec2eff1ee137
Matthew Rycraft <mattrycraft@comcast.net>
2020-04-08 10:53:53 -0500

    Project Euler Problem 6 Solution

---
+++ Problem6/Problem6Solution.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Problem6
+{
+    class Problem6Solution
+    {
+        static void Main(string[] args)
+        {
+            int sumSquared = 0, SquaredSums = 0, Difference = 0;
+
+            for (int i = 1; i <= 100; i++)
+            {
+                sumSquared += i;
+                SquaredSums += i * i;
+            }
+
+            Difference = (sumSquared * sumSquared) - SquaredSums;
+            Console.WriteLine("The difference between the square of sum and sum of squares for 1 - 100 is " + Difference) ;
+        }
+    }
+}
